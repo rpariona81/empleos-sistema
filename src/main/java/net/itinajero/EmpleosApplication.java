76b2de184d6871f4118a7a7c0e19eb03a25c8b3d
rpariona81 <ronald.pariona.isidro@gmail.com>
2021-01-26 02:45:12 -0500

    https://dzone.com/articles/deploying-spring-boot-app-to-jboss-wildfly

--- src/main/java/net/itinajero/EmpleosApplication.java
+++ src/main/java/net/itinajero/EmpleosApplication.java
@@ -2,11 +2,20 @@ package net.itinajero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class EmpleosApplication /*extends SpringBootServletInitializer*/{
+@EnableSwagger2
+public class EmpleosApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EmpleosApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication.class, args);
 	}
