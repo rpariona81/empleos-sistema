81ae3268bf2352e5367c990201a5f590c54e9180
rpariona81 <ronald.pariona.isidro@gmail.com>
2021-01-26 01:17:18 -0500

    para jar

--- src/main/java/net/itinajero/EmpleosApplication.java
+++ src/main/java/net/itinajero/EmpleosApplication.java
@@ -2,9 +2,10 @@ package net.itinajero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmpleosApplication {
+public class EmpleosApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication.class, args);
